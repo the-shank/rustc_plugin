2784cbd97a5d4a9db81326f01c1e3d755708b0a0
Shank <shashanksharma21@gmail.com>
2023-10-10 04:38:03 +0000

    updated test crate with more types

--- crates/rustc_plugin/examples/print-all-items/test-crate/src/lib.rs
+++ crates/rustc_plugin/examples/print-all-items/test-crate/src/lib.rs
@@ -1,5 +1,27 @@
+#![allow(dead_code)]
+#![allow(clippy::disallowed_names)]
+
 pub fn add(left: usize, right: usize) -> usize {
   left + right
+}
+
+pub struct Foo {
+  i: i32,
+  j: u32,
+}
+
+pub fn foo_fn(foo: Foo) -> i32 {
+  foo.i
+}
+
+use std::mem;
+
+struct Bar {
+  vec: Vec<i32>,
+}
+
+fn bar_fn() {
+  println!("size_of Bar is {}", mem::size_of::<Bar>());
 }
 
 #[cfg(test)]
